5a25528a050828d623079f2cdc1ddf3c759ed3c2
Nuno Penim <nuno.penim@gmail.com>
2020-05-18 15:23:35 +0100

    Updates to Vertex and Fragment Shaders

--- glTest/frag.glsl
+++ glTest/frag.glsl
@@ -6,16 +6,11 @@ in vec2 st;
 out vec4 FragColor;
 
 void main () {
-//    finalcolor = vec4(vec3(interpolatedNormal.z), 1.0);
-    FragColor = vec4(1,0,0,1);
-    
-    const vec3 AmbientColor = vec3(0.1, 0.0, 0.0);
-    const vec3 DiffuseColor = vec3(1.0, 0.0, 0.0);
-//
-    vec3 normalized_normal = normalize(interpolatedNormal);
-    vec3 normalized_lightDir = normalize(vec3(1.0,0.0,1.0));
-//
-    float DiffuseTerm = clamp(dot(normalized_normal, normalized_lightDir), 0.0, 1.0);
-    //FragColor = vec4(AmbientColor + DiffuseColor * DiffuseTerm, 1.0);
+    //FragColor = vec4(1,0,0,1);
+    //const vec3 AmbientColor = vec3(0.1, 0.0, 0.0);
+    //const vec3 DiffuseColor = vec3(1.0, 0.0, 0.0);
+    //vec3 normalized_normal = normalize(interpolatedNormal);
+    //vec3 normalized_lightDir = normalize(vec3(1.0,0.0,1.0));
+    //float DiffuseTerm = clamp(dot(normalized_normal, normalized_lightDir), 0.0, 1.0);
     FragColor = texture(texturePlanet, st);
 }
--- glTest/vertex.glsl
+++ glTest/vertex.glsl
@@ -14,7 +14,6 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(Position, 1.0);
-//  gl_Position = vec4(Position , 1.0); // Special , required output
     interpolatedNormal = Normal; // Will be interpolated across the triangle
     st = TexCoord; // Will also be interpolated across the triangle
 }
