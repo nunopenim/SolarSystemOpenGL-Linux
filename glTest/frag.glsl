ab9a4ee36cd4ad3d3024dcd733cfc483652c0a27
Nuno Penim <nuno.penim@gmail.com>
2020-05-18 15:34:51 +0100

    Cleanup in fragment shader and solution

--- glTest/frag.glsl
+++ glTest/frag.glsl
@@ -6,11 +6,5 @@ in vec2 st;
 out vec4 FragColor;
 
 void main () {
-    //FragColor = vec4(1,0,0,1);
-    //const vec3 AmbientColor = vec3(0.1, 0.0, 0.0);
-    //const vec3 DiffuseColor = vec3(1.0, 0.0, 0.0);
-    //vec3 normalized_normal = normalize(interpolatedNormal);
-    //vec3 normalized_lightDir = normalize(vec3(1.0,0.0,1.0));
-    //float DiffuseTerm = clamp(dot(normalized_normal, normalized_lightDir), 0.0, 1.0);
     FragColor = texture(texturePlanet, st);
 }
