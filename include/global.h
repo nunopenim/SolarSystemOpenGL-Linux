3c47b1a4e46ae1421fb8a57fb4da6c040a19155a
nunopenim <nuno.penim@gmail.com>
2020-07-22 21:14:51 +0100

    global: file restructure

--- include/global.h
+++ include/global.h
@@ -1,11 +1,14 @@
+// GL/GLFW Stuff
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
-// Include GLM
+// GLM Stuff
 #include <glm/glm.hpp>
 #include "glm/gtc/matrix_transform.hpp"
 #include <glm/gtc/type_ptr.hpp>
-#include "camera.hpp"
+
+// My stuff
+#include "Camera.hpp"
 
 #ifndef GLOBAL
 #define GLOBAL
